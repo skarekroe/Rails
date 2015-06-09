b5992b2cb1442c7c9a8c48b4b0467e798b9f8ab0
skarekroe <nebsregor@gmail.com>
2015-06-09 17:42:44 -0400

    finished blog comments feature

--- blogger/app/controllers/application_controller.rb
+++ blogger/app/controllers/application_controller.rb
@@ -1,47 +1,4 @@
 class ApplicationController < ActionController::Base
-  include ArticlesHelper
-  protect_from_forgery with: :exception
 
-	def index
-		@article = Article.all
-	end
-
-	def show
-		@article = Article.find(params[:id])
-	end
-
-	def new
-		@article = Article.new
-	end
-
-	def create
-		@article = Article.new(article_params)
-		@article.save
-
-		flash.notice = "Created Article: '#{@article.title}'!"
-
-		redirect_to article_path(@article)	
-	end
-
-	def destroy
-		@article = Article.find(params[:id]).destroy
-
-		flash.notice = "Deleted Article '#{@article.title}'!"
-
-		redirect_to articles_path
-	end
-
-	def edit
-		@article = Article.find(params[:id])
-	end
-
-	def update
-		@article = Article.find(params[:id])
-		@article.update(article_params)
-
-		flash.notice = "Article '#{@article.title}' Updated!"
-
-		redirect_to article_path(@article)
-	end
 
 end
