b25f7fdf495265bfe2c2d3f17528f47869e57021
skarekroe <nebsregor@gmail.com>
2015-06-11 18:29:19 -0400

    added a few gems

--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -2,9 +2,11 @@ class Article < ActiveRecord::Base
 	has_many :comments
 	has_many :taggings
 	has_many :tags, through: :taggings
+	has_attached_file :image
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
 	def tag_list
-		tags.join(', ')
+		self.tags.collect{|tag| tag.name}.join(", ")
 	end
 
 	def tag_list=(tags_string)
--- blogger/app/models/tag.rb
+++ blogger/app/models/tag.rb
@@ -2,7 +2,5 @@ class Tag < ActiveRecord::Base
 	has_many :taggings
 	has_many :articles, through: :taggings
 
-	def to_s
-		name
-	end
+	
 end
